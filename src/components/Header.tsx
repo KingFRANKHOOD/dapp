34376418494e6dc400ed9babc31ccbf58b48e0b9
JamesVictor-o <victorjames40v@gmail.com>
2025-02-01 08:19:19 +0100

    feat: installed required dependency

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,82 @@
-import ThemeToggle from './ThemeToggle'
 
-const Header = () => (
-  <header className='sticky top-0 z-50 w-full border-b bg-background shadow-md'>
-    <ThemeToggle />
-  </header>
-)
+import { promises } from "dns";
+import Link from "next/link";
+interface HeaderProps{
+  connectWallet:()=>Promise<void>
+}
 
-export default Header
+const Header:React.FC<HeaderProps> = ({}) => {
+    return (
+      <div className="w-full mt-4 bg-[#010c03]">
+        <nav className="bg-transparent w-full px-4">
+          <div className="flex flex-wrap items-center justify-between  p-4">
+            <div className="text-white font-Bebas text-2xl font-semibold">
+            DECLENUP impact product
+            </div>
+  
+            {/* connect wallet button */}
+            <div className=" flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+              <button className=" hidden md:flex px-4 py-2 border-2 border-[#1BE4D2] rounded-full text-xl font-bold  relative bg-transparent overflow-hidden">
+                <span className="bg-gradient-to-r from-[#1BE4D2] to-[#01FC88] bg-clip-text text-bold text-2xl text-transparent">
+                  CONNECT WALLET
+                </span>
+                <span className="absolute inset-0 bg-gradient-to-r from-[#1BE4D2] to-[#01FC88] rounded-full -z-10"></span>
+              </button>
+              <button
+                data-collapse-toggle="navbar-sticky"
+                type="button"
+                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                aria-controls="navbar-sticky"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 17 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 1h15M1 7h15M1 13h15"
+                  />
+                </svg>
+              </button>
+            </div>
+  
+            {/* navigation bar */}
+            <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
+              <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium md:space-x-8 rtl:space-x-reverse md:flex-row ">
+                 <Link   className="block py-2 px-3 text-white text-3xl font-bold rounded-sm " href={"/earn"}>
+                   EARN
+                 </Link>
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 px-3 text-white text-3xl font-bold rounded-sm "
+                  >
+                    CLEAN UP
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 px-3 text-white text-3xl font-bold rounded-sm "
+                  >
+                    SNAP
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  };
+  
+  export default Header;
+  
