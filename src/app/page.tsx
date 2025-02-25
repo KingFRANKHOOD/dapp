85a7c1762c5ff656a738459a3cee1c2fb338f54c
JamesVictor-o <victorjames40v@gmail.com>
2025-02-25 10:27:48 +0100

    made fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,8 @@ import Login from "@/components/Login";
 
 export default function Home() {
   return (
-    <div className=" overflow-hidden">
-           <Login/>
-     <HeroSection/>
-     <DecleanupDos/>
-     <HowItWorks/>
-     <Faqs/>
+    <div>
+      <Login/>
     </div>
   );
 }
