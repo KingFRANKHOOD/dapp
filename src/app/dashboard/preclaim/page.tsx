787331ebbae392dd28ea70d578c16c25e1fd3a5d
frankhood <youremail@domain.com>
2025-03-01 12:22:53 +0100

    'chore: reorganise fonts and spacing'

--- src/app/dashboard/preclaim/page.tsx
+++ src/app/dashboard/preclaim/page.tsx
@@ -6,35 +6,35 @@ import Before from '@/public/AFTER.png'
 function Preclaim() {
   return (
     <div className='flex h-screen w-full flex-col'>
-      <div className='h-1/4 w-full bg-white bg-opacity-0 sm:h-1/3'>
-        <button className='absolute right-8 top-4 scale-x-[1.5] font-sans text-4xl font-light text-white'>
+      <div className='h-1/4 w-full bg-white bg-opacity-0 md:h-1/3'>
+        <button className='absolute right-4 md:right-8 top-4 scale-x-[1.5] font-sans text-4xl font-light text-white'>
           X
         </button>
       </div>
-      <div className='relative h-3/4 w-[100%] overflow-hidden bg-[#FAFF00] sm:h-2/3'>
-        <div className='absolute top-4 ml-4 flex flex-row gap-3 text-xl sm:gap-6'>
+      <div className='relative h-3/4 w-[100%] overflow-hidden bg-[#FAFF00] md:h-2/3'>
+        <div className='absolute top-4 ml-4 flex flex-row gap-3 text-xl md:gap-6'>
           <div className='font-sans font-bold text-black'>
             <span className=''>1. BEFORE</span>
-            <div className='mt-5 sm:mt-8'>
+            <div className='mt-5 md:mt-8'>
               <Image src={After} alt='Profile' width={335} height={304} />
             </div>
           </div>
           <div className='mr-3 flex-row font-sans font-bold text-black'>
             <span>2. AFTER</span>
-            <div className='mt-5 sm:mt-8'>
+            <div className='mt-5 md:mt-8'>
               <Image src={Before} alt='' width={335} height={304} />
             </div>
           </div>
         </div>
-        <div className='absolute bottom-56 flex w-full scale-y-150 flex-col text-left font-bebas leading-tight text-black sm:bottom-auto sm:right-4 sm:top-10 sm:w-1/4'>
-          <div className='absolute left-4 sm:right-6'>
+        <div className='absolute bottom-56 flex w-full scale-y-150 flex-col text-left font-bebas text-black md:bottom-auto md:right-4 md:top-10 md:w-1/4'>
+          <div className='leading-6 tracking-normal md:text-xl md:font-normal absolute left-4 md:right-6'>
             <span>
               AFTER THE TEAM REVIEW THE PROOF OF CLEANUP, COME BACK TO CLAIM
               YOUR NEW LEVEL. USUALLY THE PROCESS TAKES FROM 2 TO 12 HOURS.
               CONTACT US IN TELEGRAM GROUP IF YOU HAVE QUESTIONS OR FOR
               TROUBLESHOOTING
             </span>
-            <div className='mt-3 text-center font-sans text-2xl font-bold sm:right-6 sm:mt-28 sm:text-3xl'>
+            <div className='mt-3 text-center font-sans text-2xl font-bold md:right-6 md:mt-20 md:text-3xl'>
               IN REVIEW
             </div>
           </div>
